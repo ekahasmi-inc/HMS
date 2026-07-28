4db9285bceead9be93de06d99ab1f3f96bedfbf0
Narendra Kumar <narendrak.amu@gmail.com>
2026-07-28 20:49:38 +0530

    feat(identity): implement role model

--- backend/apps/platform/identity/admin.py
+++ backend/apps/platform/identity/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-
+from .models import Role
 from .models import User
 
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
     model = User
+
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+
+    list_display = ("name", "code", "is_system_role", "is_active",)
+    list_filter = ("is_system_role", "is_active",)
+    search_fields = ("name", "code",)
--- backend/apps/platform/identity/models.py
+++ backend/apps/platform/identity/models.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import AbstractUser
-
+from django.db import models
 from apps.platform.common.models import BaseModel
 from .managers import CustomUserManager
 
@@ -13,3 +13,23 @@ class User(BaseModel, AbstractUser):
         db_table = "identity_users"
         verbose_name = "User"
         verbose_name_plural = "Users"
+
+
+class Role(BaseModel):
+    """
+    Defines user roles in the platform.
+    """
+    name = models.CharField(max_length=100, unique=True,)
+    code = models.SlugField(max_length=100, unique=True, db_index=True,)
+    description = models.TextField(blank=True,)
+    is_system_role = models.BooleanField(default=False, help_text="Predefined roles that cannot be deleted.",)
+    is_active = models.BooleanField(default=True, db_index=True,)
+
+    class Meta:
+        db_table = "roles"
+        ordering = [
+            "name",
+        ]
+
+    def __str__(self):
+        return self.name
